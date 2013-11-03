081d7c13afdd79133320fe3823bcfb5c948009aa
Patrick Seebauer <patrick.seebuaer@web.de>
2013-11-03 19:44:54 +0100

    added warning when two subs refer to the same handler

--- lib/XML/Parser/Class.pm
+++ lib/XML/Parser/Class.pm
@@ -2,34 +2,33 @@ package XML::Parser::Class;
 
 use true;
 use parent XML::Parser::Expat;
+use Carp;
 
 sub new {
   my($package) = shift;
   my @st;
+  my %dispatch;
   while (my ($symbol_table_key, $val) = each %{ *{ "$package\::" } }) {
     local *ENTRY = $val;
-    push @st,[$symbol_table_key, *ENTRY{ CODE }] if defined $val and defined *ENTRY{ CODE };
-  }
-  my %dispatch;
-  foreach (@st){
-    if ($_->[0] =~ /^(?:(?'what'Start|End)_?(?'who'.*)
+    if (defined $val 
+	and defined *ENTRY{ CODE }
+	and $symbol_table_key =~ /^(?:(?'what'Start|End)_?(?'who'.*)
 		    |(?'who'.*?)_?(?'what'handler))$/x){
-      $dispatch{$+{what}}{$+{who}}=$_->[1];
+      $dispatch{$+{what}}{$+{who}}= *ENTRY{ CODE };
     }
   }
-  my $s = bless(XML::Parser::Expat->new,$package);
+  my $s = bless(XML::Parser::Expat->new(@_),$package);
 # not sure if reblessing is appropriate here... otherwise i'd have to go the AUTOLOAD-route and then I'll have to do some ugly switches in the dispatch methods.
   $s->setHandlers($s->__gen_dispatch(\%dispatch));
   return $s;
 }
 
 sub __gen_dispatch{
-  die "Do you know what privacy means?" if (caller)[0] ne __PACKAGE__;
   my ($s,$dispatch) = @_;
   my %ret;
   foreach my $se (qw|Start End|) {
     if ($dispatch->{$se}) {
-      if (not $s->can('transform_gi')) {
+      if (not $s->can('transform_gi')) { # the alternative would be to have a generic transform_gi sub, i don't want that, because it's much slower.
 	$ret{$se} = sub {
 	  if ($dispatch->{$se}{$_[1]}) {
 	    $dispatch->{$se}{$_[1]}->(@_);
@@ -39,8 +38,11 @@ sub __gen_dispatch{
 	}
       } else {
 	foreach (keys %{$dispatch->{$se}}) {
-	  if ($_ ne $s->transform_gi($_)){
-	    $dispatch->{$se}{$s->transform_gi($_)} = $dispatch->{$se}{$_};
+	  my $new_key=$s->transform_gi($_);
+	  if ($_ ne $new_key){
+	    carp "the handlers for $_ and $new_key are identical"
+	      if exists $dispatch->{$se}{$new_key};
+	    $dispatch->{$se}{$new_key} = $dispatch->{$se}{$_};
 	    delete $dispatch->{$se}{$_};
 	  }
 	}
