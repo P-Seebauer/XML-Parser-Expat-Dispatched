391896f0dc3f4932f5514da1bb32e78a6ad8ba07
Patrick Seebauer <patrick.seebuaer@web.de>
2013-11-03 20:01:19 +0100

    verbose error message

--- lib/XML/Parser/Class.pm
+++ lib/XML/Parser/Class.pm
@@ -14,7 +14,7 @@ sub new {
 	and defined *ENTRY{ CODE }
 	and $symbol_table_key =~ /^(?:(?'what'Start|End)_?(?'who'.*)
 		    |(?'who'.*?)_?(?'what'handler))$/x){
-      $dispatch{$+{what}}{$+{who}}= *ENTRY{ CODE };
+      $dispatch{$+{what}}{$+{who}}= [*ENTRY{ CODE }, $symbol_table_key];
     }
   }
   my $s = bless(XML::Parser::Expat->new(@_),$package);
@@ -31,16 +31,16 @@ sub __gen_dispatch{
       if (not $s->can('transform_gi')) { # the alternative would be to have a generic transform_gi sub, i don't want that, because it's much slower.
 	$ret{$se} = sub {
 	  if ($dispatch->{$se}{$_[1]}) {
-	    $dispatch->{$se}{$_[1]}->(@_);
+	    $dispatch->{$se}{$_[1]}[0](@_);
 	  }elsif(defined $dispatch->{$se}{''}){
-	    $dispatch->{$se}{''}(@_);
+	    $dispatch->{$se}{''}[0](@_);
 	  }
 	}
       } else {
 	foreach (keys %{$dispatch->{$se}}) {
 	  my $new_key=$s->transform_gi($_);
 	  if ($_ ne $new_key){
-	    carp "the handlers for $_ and $new_key are identical"
+	    carp "there is already an handler for \l$se $new_key installed, overwriting it with $dispatch->{$se}{$_}[1]"
 	      if exists $dispatch->{$se}{$new_key};
 	    $dispatch->{$se}{$new_key} = $dispatch->{$se}{$_};
 	    delete $dispatch->{$se}{$_};
@@ -48,16 +48,16 @@ sub __gen_dispatch{
 	}
 	$ret{$se} = sub {
 	  if ($dispatch->{$se}{$s->transform_gi($_[1])}) {
-	    $dispatch->{$se}{$s->transform_gi($_[1])}->(@_);
+	    $dispatch->{$se}{$s->transform_gi($_[1])}[0](@_);
 	  }elsif(defined $dispatch->{$se}{''}){
-	    $dispatch->{$se}{''}(@_);
+	    $dispatch->{$se}{''}[0](@_);
 	  }
 	}
       }
     }
   }
   foreach my $handler (keys %{$dispatch->{handler}}){
-    $ret{$handler} = sub{$dispatch->{handler}{$handler}->($s,$_[1..$#_])}
+    $ret{$handler} = sub{$dispatch->{handler}{$handler}[0]($s,$_[1..$#_])}
   }
   return %ret;
 }
